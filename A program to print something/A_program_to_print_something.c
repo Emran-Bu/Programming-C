3b389c20e89704135ca6e2af1b8695c51d2a7c62
Freelancer-Emran <mdemranhasan809@gmail.com>
2022-07-29 22:43:40 +0600

    first printf program in c

---
+++ A program to print something/A_program_to_print_something.c
@@ -0,0 +1,9 @@
+#include<stdio.h>   //standard input output system (header file / library)
+#include<conio.h>   //declare console input output system (header file / library)
+
+void main()     //main funtion
+{
+    printf("This is my first program\n");  // A program to print something text
+    printf("1220 12.13 1100000");          // A program to print something number
+    getch();    //This method pauses the Output Console until a key is pressed
+}
