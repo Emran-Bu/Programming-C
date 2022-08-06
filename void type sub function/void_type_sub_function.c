346f39f53bb91c23fc034e7daaf350a72475567c
Freelancer-Emran <mdemranhasan809@gmail.com>
2022-08-06 09:38:32 +0600

    void type sub function

---
+++ void type sub function/void_type_sub_function.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+
+void subFunForSum(int, int); //parameterized sub function prototype
+void subFunForSub(); //non parameterized sub function prototype
+void subFunForSub();
+void main()
+{
+    //printf("%d", subFun(10));
+    subFunForSum(10, 10);
+    subFunForSub();
+    subFunForArr();
+    getch();
+}
+
+void subFunForSum(int a, int b) //parameterized sub function
+{
+    int c = a + b;
+    printf("subFunForSum = %d\n", c);
+}
+
+void subFunForSub() //non parameterized sub function
+{
+    int a = 20;
+    int b = 10;
+    int c = b-c;
+    printf("subFunForSub = %d\n", c);
+}
+
+void subFunForArr()
+{
+    int arr[] = {3,1,5,7};
+    int i;
+    for(i=0;i<=3;i++)
+    {
+        printf("array[%d] = %d\n", i, arr[i]);
+    }
+
+}
